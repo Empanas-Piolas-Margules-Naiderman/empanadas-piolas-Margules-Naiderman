d96120d1a1153ad64e61427a84dec66fdce447f7
MarguJ <margulesjoa@gmail.com>
2026-05-13 18:22:11 -0300

    Esperemos que funcione

--- back/src/services/orderService.js
+++ back/src/services/orderService.js
@@ -1,1 +1,17 @@
 import { supabase } from "../../supabase";
+
+export async function AddProduct(gusto, amount) {
+  const { data, error } = await supabase
+    .from("order")
+    .update({ cantidad: amount })
+    .eq("gusto", gusto);
+
+  if (error) {
+    console.error("No se pudo añadir el producto");
+  }
+  console.log("Producto añadido");
+}
+
+export async function GetOrder() {
+  const { data, error } = await supabase.from("order").select("*");
+}
